502b60820c03070154d44153a2770bfc41b72b7a
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-14 20:48:42 +0300

    first add VendingMachine.java

--- OopSem1/VendingMachine.java
+++ OopSem1/VendingMachine.java
@@ -1,5 +1,17 @@
 package OopSem1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendingMachine {
-    
+    List<Product> list = new ArrayList<>();
+    public VendingMachine addProduct(Product product){
+        list.add(product);
+        return this;
+
+    }
+
+    public List<Product> getList() {
+        return list;
+    }
 }
