89e4a52b7d50530612b28db77349c4692e48701a
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-14 20:59:18 +0300

    fourth add main.java

--- OopSem1/Main.java
+++ OopSem1/Main.java
@@ -6,7 +6,6 @@ public class Main {
         vendingMachine.addProduct(new Product("cheese",300))
                         .addProduct(new Product("water", 10))
                 .addProduct(new Product("wine", 330));
-                .addProduct(new Yogurt("Danone", 300, "sweet"));
         for (int i = 0; i < vendingMachine.getList().size(); i++) {
             System.out.println(vendingMachine.getList().get(i));
         }
