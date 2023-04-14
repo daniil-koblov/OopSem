2d615071341064adcfe87626d4a8a55ab6385c44
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-14 20:51:57 +0300

    first add Yogurt.java

--- OopSem1/Yogurt.java
+++ OopSem1/Yogurt.java
@@ -1,5 +1,12 @@
 package OopSem1;
 
-public class Yogurt {
-    
+public class Yogurt extends Product {
+    public Yogurt(String name, Integer cost, String flavor){
+        super(name, cost);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
