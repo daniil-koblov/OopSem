0644e6ed1763f2939ed85abe1a863631008ff176
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-14 20:35:38 +0300

    second add Product.java

--- OopSem1/Product.java
+++ OopSem1/Product.java
@@ -1,5 +1,24 @@
 package OopSem1;
 
 public class Product {
-    
+    public void setName(String name){
+        this.name = name;
+    }
+    private String name;
+
+    public void setCost(Integer cost){
+        this.cost = cost;
+    }
+
+    private Integer cost;
+
+    public Product(String name, Integer cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString("%s, %d");
+    }
 }
