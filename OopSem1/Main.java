1aa7c1451ee09a4d7fe6c739214e307c621291fb
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-14 20:49:20 +0300

    second add main.java

--- OopSem1/Main.java
+++ OopSem1/Main.java
@@ -2,6 +2,13 @@ package OopSem1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.addProduct(new Product("cheese",300))
+                        .addProduct(new Product("water", 10))
+                .addProduct(new Product("wine", 330));
+        for (int i = 0; i < vendingMachine.getList().size(); i++) {
+            System.out.println(vendingMachine.getList().get(i));
+        }
+
     }
 }
