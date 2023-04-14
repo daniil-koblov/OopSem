65df107234b7def0744728872515b848dc9675cb
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-14 21:08:46 +0300

    first add Perfume.java

--- OopSem1/Perfume.java
+++ OopSem1/Perfume.java
@@ -1,5 +1,15 @@
 package OopSem1;
 
-public class Perfume {
-    
+public class Perfume extends Product{
+
+    private final String brand;
+    public Perfume(String name, Integer cost, String brand) {
+        super(name, cost);
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s",super.toString(), brand);
+    }
 }
