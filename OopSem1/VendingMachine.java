02ec369081aa32328d72372dc379b9acdea44fc2
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-17 17:16:07 +0300

    second add first add VendingMachine.java

--- OopSem1/VendingMachine.java
+++ OopSem1/VendingMachine.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class VendingMachine {
     List<Product> list = new ArrayList<>();
+
+    public int getAmount() {
+        return amount;
+    }
+
+    private int amount = 0;
     public VendingMachine addProduct(Product product){
         list.add(product);
         return this;
@@ -13,5 +19,29 @@ public class VendingMachine {
 
     public List<Product> getList() {
         return list;
+    }
+
+    public Product findProduct(String string){
+        for (int i = 0; i < getList().size(); i++) {
+            if (getList().get(i).getName().equals(string)){
+                return getList().get(i);
+            }
+        }
+        return null;
+    }
+
+    public Product sellProduct(Product product){
+        try{
+            Product prodFound = findProduct(product.getName());
+            getList().remove(prodFound);
+            amount += prodFound.getCost();
+            return prodFound;
+
+        } catch(Exception ex) {
+            throw new RuntimeException("Product not found");
+
+        }
+
+
     }
 }
