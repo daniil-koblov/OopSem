8d104206753ce9dca3c8718f440f4385968f658c
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-18 20:12:18 +0300

    first add Animal.java

--- OopSem2/Animal.java
+++ OopSem2/Animal.java
@@ -1,5 +1,12 @@
 package OopSem2;
 
-public class Animal {
-    
+public abstract class Animal {
+    private String name;
+
+    public abstract String say();
+
+    @Override
+    public String toString() {
+        return String.format("Name %s: say %s", name, say());
+    }
 }
