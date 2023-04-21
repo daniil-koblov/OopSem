861fca6a081e399b23160679a1cc98c97ce20dcb
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 10:58:38 +0300

    first add Duck.java

--- OopSem2/Duck.java
+++ OopSem2/Duck.java
@@ -1,5 +1,23 @@
 package OopSem2;
 
-public class Duck {
-    
+public class Duck extends Herbivores implements Walkable, Flyable{
+    public Duck(String name) {
+        super(name);
+    }
+
+
+    @Override
+    public String say() {
+        return "Krya-krya-krya";
+    }
+
+    @Override
+    public int speedForFly() {
+        return 80;
+    }
+
+    @Override
+    public int speedForWalk() {
+        return 5;
+    }
 }
