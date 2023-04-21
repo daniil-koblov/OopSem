10613f458075afbc9f378e56af05c43f4d7debdc
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 10:59:40 +0300

    first add Hare.java

--- OopSem2/Hare.java
+++ OopSem2/Hare.java
@@ -1,5 +1,22 @@
 package OopSem2;
 
-public class Hare {
-    
+public class Hare extends Herbivores implements Walkable{
+    public Hare(String name) {
+        super(name);
+    }
+
+    @Override
+    public String say() {
+        return "Pipipi";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Hare, %s", super.toString());
+    }
+
+    @Override
+    public int speedForWalk() {
+        return 25;
+    }
 }
