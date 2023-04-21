7f59980474102a68edbf7daa3abf1b01bf7299f1
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 11:00:07 +0300

    first add Main.java

--- OopSem2/Main.java
+++ OopSem2/Main.java
@@ -1,5 +1,31 @@
 package OopSem2;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        zoo.setRadio(new Radio());
+
+        zoo.addList(new Wolf("Wolf"))
+                .addList(new Hare("Hare"))
+                .addList(new Duck("DonaldDuck"))
+                .addList(new Bat("Batman"));
+        for (var item: zoo.getListAnimal()) {
+            System.out.println(item);
+        }
+        for(var item: zoo.getSayble()){
+            System.out.println(item.say());
+        }
+        System.out.println("------------------");
+        for (var item: zoo.walkables()) {
+            System.out.printf("%s - %d\n", item.toString(), item.speedForWalk());
+        }
+        System.out.println("-----------------");
+        for (var item: zoo.flyables()) {
+            System.out.printf("%s - %d\n", item.toString(), item.speedForFly());
+        }
+        System.out.println("----------------");
+        System.out.println("Winner " + zoo.winnerWalk());
+        System.out.println("-----------------");
+        System.out.println("Winner " + zoo.winnerFly());
+    }
 }
