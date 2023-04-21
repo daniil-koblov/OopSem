8d1f420e2b12ac8cda38cf5770c7ed1a4354daff
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 21:27:36 +0300

    first add Company.java

--- OopSem3/Company.java
+++ OopSem3/Company.java
@@ -1,5 +1,43 @@
 package OopSem3;
 
-public class Company {
-    
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Company implements Iterable<User> {
+    private User user;
+
+    public Company(User user) {
+        this.user = user;
+    }
+
+    private List<User> deepTree(User user) {
+        List<User> subordinates = new ArrayList<>();
+        subordinates.add(user);
+        if (user.getSubordinate() == null || user.getSubordinate().size() == 0) {
+            return subordinates;
+        }
+        for (User item : user.getSubordinate()) {
+            subordinates.addAll(deepTree(item));
+        }
+        return subordinates;
+    }
+
+    @Override
+    public Iterator<User> iterator() {
+
+        return new Iterator<User>() {
+            List<User> iterUser = deepTree(user);
+            Iterator<User> iterator = iterUser.iterator();
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public User next() {
+                return iterator.next();
+            }
+        };
+    }
 }
