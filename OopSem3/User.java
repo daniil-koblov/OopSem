deea213f6f839027c5760755e80ab88185925efe
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 20:07:54 +0300

    first add User.java

---
+++ OopSem3/User.java
@@ -0,0 +1,20 @@
+package OopSem3;
+
+public class User {
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return String.format("First name: %s, Last name: %s, age: %d", firstName, lastName, age);
+    }
+}
+
