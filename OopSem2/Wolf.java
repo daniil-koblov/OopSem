8708e67fa04bfdd3723b9ae5ec560d048462dd38
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 11:03:18 +0300

    first add Wlof.java

--- OopSem2/Wolf.java
+++ OopSem2/Wolf.java
@@ -1,5 +1,22 @@
 package OopSem2;
 
-public class Wolf {
-    
+public class Wolf extends Predator implements Walkable{
+    public Wolf(String name) {
+        super(name);
+    }
+
+    @Override
+    public String say() {
+        return "Uuuu";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Wolf, %s", super.toString());
+    }
+
+    @Override
+    public int speedForWalk() {
+        return 40;
+    }
 }
