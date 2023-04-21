a330467d820de351baebd71014baee304d26f1ce
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 20:13:31 +0300

    first add Personal.java

--- OopSem3/Personal.java
+++ OopSem3/Personal.java
@@ -1,5 +1,30 @@
 package OopSem3;
 
-public class Personal {
-    
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class Personal implements Iterable<User>{
+    List<User> users = new ArrayList<>();
+
+    @Override
+    public Iterator<User> iterator() {
+        return new Iterator<User>() {
+            int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < users.size();
+            }
+
+            @Override
+            public User next() {
+                return users.get(index++);
+            }
+        };
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
 }
