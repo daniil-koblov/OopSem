405023eacda58a6252c520f465ecef6988e39665
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 21:28:53 +0300

    third add Personal.java

--- OopSem3/Personal.java
+++ OopSem3/Personal.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
-public class Personal implements Iterable<User>{
+public class Personal implements Iterable<User>
+ {
     List<User> users = new ArrayList<>();
 
-    @Override
+
     public Iterator<User> iterator() {
         return new Iterator<User>() {
             int index = 0;
@@ -22,10 +22,20 @@ public class Personal implements Iterable<User>{
                 return users.get(index++);
             }
         };
-    }
 
+    }
     public Personal addUser(User user){
         users.add(user);
         return this;
+    }
+    public int size(){
+        return users.size();
+    }
+    public List<User> toList(){
+        List<User> results = new ArrayList<>();
+        for(User user: this){
+            results.add(user);
+        }
+        return results;
     }
 }
