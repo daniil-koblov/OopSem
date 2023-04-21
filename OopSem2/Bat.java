26a219352c4de3b78646e56548523e48fbfbe210
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 10:58:14 +0300

    first add Bat.java

--- OopSem2/Bat.java
+++ OopSem2/Bat.java
@@ -1,5 +1,19 @@
 package OopSem2;
 
-public class Bat {
-    
+public class Bat extends Predator implements Flyable{
+
+    public Bat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String say() {
+        return "Pip-pip-pip";
+    }
+
+    @Override
+    public int speedForFly() {
+        return 120;
+    }
+
 }
