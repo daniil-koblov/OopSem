8071bdd7af5c5bd477e62ba59f6ae51844c8721d
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 21:29:28 +0300

    second add User.java

--- OopSem3/User.java
+++ OopSem3/User.java
@@ -1,20 +1,39 @@
 package OopSem3;
 
-public class User {
+public class User implements  Comparable<User>{
     private String firstName;
-
     private String lastName;
-
-    private int age;
+    private  int age;
+    private Personal subordinate;
 
     public User(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
+
+    public void setSubordinate(Personal subordinate) {
+        this.subordinate = subordinate;
+    }
+
+    public Personal getSubordinate() {
+        return subordinate;
+    }
+
     @Override
     public String toString() {
-        return String.format("First name: %s, Last name: %s, age: %d", firstName, lastName, age);
+        return String.format("\nFirst name: %s, Last name: %s, age: %d", firstName, lastName, age);
     }
+
+    @Override
+    public int compareTo(User o) {
+        int compareNames = this.firstName.compareTo(o.firstName);
+        if(compareNames !=0) return compareNames;
+        int compareLastNames = this.lastName.compareTo(o.lastName);
+        if(compareLastNames != 0) return compareLastNames;
+        //        return Integer.compare(this.age, o.age);
+        return this.age - o.age;
+    }
+
 }
 
