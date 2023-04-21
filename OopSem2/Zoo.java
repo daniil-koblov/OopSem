54ee7c35f2342d042f6e32076483ea5925fd40a8
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-21 11:03:47 +0300

    first add Zoo.java

--- OopSem2/Zoo.java
+++ OopSem2/Zoo.java
@@ -1,5 +1,62 @@
 package OopSem2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zoo {
-    
+    private List<Animal> listAnimal = new ArrayList<>();
+
+    public List<Animal> getListAnimal(){
+        return listAnimal;
+    }
+    public Zoo addList(Animal animal){
+        listAnimal.add(animal);
+        return this;
+    }
+    private Radio radio;
+
+    public void setRadio(Radio radio) {
+        this.radio = radio;
+    }
+    public List<Sayable> getSayble(){
+        List<Sayable> sayables = new ArrayList<>();
+        for (var item: listAnimal) {
+            sayables.add(item);
+        }
+        sayables.add(radio);
+        return sayables;
+    }
+
+    public List<Walkable> walkables(){
+        List<Walkable> walkables = new ArrayList<>();
+        for (var item:listAnimal) {
+            if(item instanceof Walkable)
+                walkables.add((Walkable) item);
+        }
+        return walkables;
+    }
+    public List<Flyable> flyables(){
+        List<Flyable> flyables = new ArrayList<>();
+        for (var item:listAnimal) {
+            if(item instanceof Flyable)
+                flyables.add((Flyable) item);
+        }
+        return flyables;
+    }
+    public Walkable winnerWalk(){
+        int index = 0;
+        for (int i = 0; i < walkables().size(); i++){
+            if(walkables().get(i).speedForWalk() > walkables().get(index).speedForWalk())
+                index = i;
+        }
+        return walkables().get(index);
+    }
+    public Flyable winnerFly(){
+        int index = 0;
+        for (int i = 0; i < flyables().size(); i++){
+            if(flyables().get(i).speedForFly() > flyables().get(index).speedForFly())
+                index = i;
+        }
+        return flyables().get(index);
+    }
 }
