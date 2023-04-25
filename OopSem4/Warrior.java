d45ce5743c7dc3c2023a38b01fb577bf08cc86ff
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-25 20:38:59 +0300

    first add Warrior.java

--- OopSem4/Warrior.java
+++ OopSem4/Warrior.java
@@ -1,5 +1,27 @@
 package OopSem4;
 
-public class Warrior {
-    
+import java.util.Random;
+
+public abstract class Warrior {
+    private String name;
+
+    private Weapon weapon;
+
+    private int healthPoint;
+
+    public Warrior(String name, Weapon weapon, int healthPoint) {
+        this.name = name;
+        this.weapon = weapon;
+        this.healthPoint = healthPoint;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name %s, weapon %s, healthPoint %d", name, weapon, healthPoint);
+    }
+
+    public int hit() {
+        Random random = new Random();
+        return random.nextInt(weapon.damage());
+    }
 }
