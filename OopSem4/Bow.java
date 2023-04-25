ea003297e85bc0c085497b8a865b739d16cf2e55
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-25 20:48:32 +0300

    first add Bow.java

--- OopSem4/Bow.java
+++ OopSem4/Bow.java
@@ -1,5 +1,17 @@
 package OopSem4;
 
-public class Bow {
-    
+public class Bow implements Weapon{
+    @Override
+    public int damage() {
+        return 20;
+    }
+
+    public int range(){
+        return 50;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Bow, max damage %d, max range %d", damage(), range());
+    }
 }
