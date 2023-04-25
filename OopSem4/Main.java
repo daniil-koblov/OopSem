50fb3c6443066f77f90c09d03e46ca38a4808060
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-25 21:27:04 +0300

    create team

--- OopSem4/Main.java
+++ OopSem4/Main.java
@@ -1,5 +1,17 @@
 package OopSem4;
 
 public class Main {
+    public static void main(String[] args) {
+        Team<Archer> archersTeam = new Team<>();
+        archersTeam.add(new Archer("George", new Bow(), 30))
+                    .add(new Archer("Bob", new Bow(), 25));
+        
+        Team<Assasin> assasinTeam = new Team<>();
+        assasinTeam.add(new Assasin("Ecio",new Knife(), 20))
+                    .add(new Assasin("Maria", new Knife(), 35));
+
+        System.out.println(archersTeam);
+        System.out.println(assasinTeam);
+    }
     
 }
