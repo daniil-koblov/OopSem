87b2036d3d07274fac124b021f4476ad8b88061f
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-25 20:59:16 +0300

    create Archer.java

---
+++ OopSem4/Archer.java
@@ -0,0 +1,13 @@
+package OopSem4;
+
+import java.util.Random;
+
+public class Archer extends Warrior{
+    public Archer(String name, Bow bow, int healthPoint) {
+        super(name, bow, healthPoint);
+    }
+    int range(){
+        Random random = new Random();
+        return random.nextInt(((Bow)weapon).range());
+    }
+}
