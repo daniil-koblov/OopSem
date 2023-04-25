e3693a613b34413bd4ed4c6bee44c1f17fb91c4e
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-04-25 20:44:42 +0300

    first add Knife.java

--- OopSem4/Knife.java
+++ OopSem4/Knife.java
@@ -1,5 +1,13 @@
 package OopSem4;
 
-public class Knife {
-    
+public class Knife implements Weapon {
+    @Override
+    public int damage() {
+        return 15;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Knife, max damage %d", damage());
+    }
 }
