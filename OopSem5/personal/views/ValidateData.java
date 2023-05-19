06035449b8da9e27f91bc87c08be2da986d5df64
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-05-19 16:49:07 +0300

    first add ValidateData.java

--- OopSem5/personal/views/ValidateData.java
+++ OopSem5/personal/views/ValidateData.java
@@ -1,5 +1,20 @@
 package OopSem5.personal.views;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidateData {
-    
+
+    String patternName = "^\\S+$";
+    Pattern pattern = Pattern.compile(patternName, Pattern.MULTILINE);
+
+    public boolean checkFirstName(String firstName){
+        Matcher matcher = pattern.matcher(firstName);
+        return !matcher.find();
+    }
+    public boolean checkLastName(String firstName){
+        Matcher matcher = pattern.matcher(firstName);
+        return !matcher.find();
+    }
+
 }
